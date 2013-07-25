ee18c57d0de4baced4171e64317d9de79bf80cd1
Grant T. Olson <kgo@grant-olson.net>
2013-07-25 06:17:35 -0400

    Show name from address book

---
+++ address_store.rb
@@ -0,0 +1,30 @@
+require_relative 'xmlrpc_client.rb'
+require 'json'
+require 'base64'
+
+class AddressStore
+  attr_reader :addresses
+
+  def initialize
+    @client = XmlrpcClient.new
+    @addresses = {}
+    update
+  end
+  
+  def log x
+    puts x
+  end
+
+  def update
+    address_infos = JSON.parse(@client.listAddresses)['addresses']
+    address_infos.each do |address_info|
+      address = address_info['address']
+      if @addresses.has_key? address
+        log "Already seen #{address}, skipping..."
+      else
+        @addresses[address] = address_info
+        log "Added #{address}."
+      end
+    end
+  end
+end
--- sinatra_server.rb
+++ sinatra_server.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'haml'
 require_relative 'message_store.rb'
+require_relative 'address_store.rb'
 
 $message_store = MessageStore.new
+$address_store = AddressStore.new
 
 get "/", :provides => :html do
   $message_store.update
   @messages = $message_store.by_recipient
+  @addresses = $address_store.addresses
   haml :threaded_messages, :layout => :layout
 end

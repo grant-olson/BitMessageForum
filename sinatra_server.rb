4f03d40728e10307252c9d2115628581227b557c
Grant T. Olson <kgo@grant-olson.net>
2013-07-25 07:39:14 -0400

    It's not a mail program, it's a forum

--- sinatra_server.rb
+++ sinatra_server.rb
@@ -6,31 +6,58 @@ require_relative 'address_store.rb'
 $message_store = MessageStore.new
 $address_store = AddressStore.new
 
+def folder folder_name
+  case folder_name
+  when "chans"
+    $message_store.chans
+  when "inbox"
+    $message_store.inbox
+  when "lists"
+    $message_store.lists
+  else
+    raise "BADFOLDER"
+  end
+end
+
+
 get "/", :provides => :html do
   $message_store.update
-  @messages = $message_store.inbox
+  @messages = folder("inbox")
   @addresses = $address_store.addresses
   haml :threaded_messages, :layout => :layout
 end
 
-get "/inbox", :provides => :html do
+get "/:folder/", :provides => :html do
   $message_store.update
-  @messages = $message_store.inbox
+  @messages = folder params[:folder]
+  
   @addresses = $address_store.addresses
-  haml :threaded_messages, :layout => :layout
+  haml :addresses, :layout => :layout
 end
 
-get "/lists", :provides => :html do
+get "/:folder/:address/", :provides => :html do
   $message_store.update
-  @messages = $message_store.lists
+  
+  @address, @threads = folder(params[:folder]).detect {|address, threads| address == params[:address] }
   @addresses = $address_store.addresses
-  haml :threaded_messages, :layout => :layout
+
+  haml :threads, :layout => :layout
+  
 end
 
-get "/chans", :provides => :html do
+get "/:folder/:address/:thread", :provides => :html do
   $message_store.update
-  @messages = $message_store.chans
+  
+  @address, threads = folder(params[:folder]).detect {|address, threads| address == params[:address] }
+  @thread, @messages = threads.detect do |thread, messages|
+    # puts messages.inspect
+    thread == CGI.unescape(params[:thread])
+  end
+  
+  
   @addresses = $address_store.addresses
-  haml :threaded_messages, :layout => :layout
+
+  haml :messages, :layout => :layout
+  
 end
 

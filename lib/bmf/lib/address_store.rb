1bc9d5025dcb3b3b8d4af88b3e9682c90688ca95
Grant T. Olson <kgo@grant-olson.net>
2013-08-24 15:23:18 -0400

    use listAddressbook if API supports it

--- lib/bmf/lib/address_store.rb
+++ lib/bmf/lib/address_store.rb
@@ -23,8 +23,20 @@ class BMF::AddressStore
     puts x
   end
 
-  def update
-    address_infos = JSON.parse(BMF::XmlrpcClient.instance.listAddresses)['addresses']
+  def update_address_list method
+    address_text = BMF::XmlrpcClient.instance.send(method)
+    if BMF::XmlrpcClient.is_error? address_text
+      puts "Couldn't call #{method}. #{address_text}"
+
+      if method == :listAddressbook
+        puts "This is expected because listAddressbook isn't in the official release yet."
+        puts "Merge pull request #429 into PyBitmessage to add it."
+      end
+      
+      return
+    end
+    
+    address_infos = JSON.parse(address_text)['addresses']
 
     lock = Mutex.new
 
@@ -32,6 +44,10 @@ class BMF::AddressStore
       new_addresses = 0
 
       address_infos.each do |address_info|
+        if address_info[-1] == "\n" # Probably base64
+          address_info['label'] = Base64.decode64(address_info['label'])
+        end
+        
         address = address_info['address']
         if !@addresses.has_key? address
           new_addresses += 1
@@ -41,5 +57,9 @@ class BMF::AddressStore
 
       log "Added #{new_addresses} addresses..." if new_addresses > 0
     end
+  end
+
+  def update
+    [:listAddresses, :listAddressbook].each { |m| update_address_list(m) }
   end
 end
